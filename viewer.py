b4f29481ba6dad52c355e3eafb55224219318d41
Gavin Barnett <gavbarnett@gmail.com>
2019-09-18 20:40:40 +0100

    Hz exp/ln & fixed mean calcs

--- viewer.py
+++ viewer.py
@@ -8,16 +8,16 @@ quant_v = 10
 
 def main():
     p = pyaudio.PyAudio()
-    fs = 42000       # sampling rate, Hz, must be integer
+    fs = 42000 # sampling rate, Hz, must be integer
 
     cap = cv2.VideoCapture(0)
     #generate tones
     fmax = 10000
     fmin = 500
-    fdiv = (fmax-fmin)/quant_v
+    fdiv = (math.log(fmax)-math.log(fmin))/quant_v
     t = []
     for n in reversed(range(quant_v)):
-        t.append(tonemaker(fmin + fdiv*n, fs))
+        t.append(tonemaker(math.exp(math.log(fmin) + fdiv*n), fs))
   
     stream = p.open(format=pyaudio.paFloat32,
                 channels=2,
@@ -28,7 +28,7 @@ def main():
         ret, frame = cap.read()
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         horizontal_img = gray #cv2.flip(gray, 1)
-        height, width = horizontal_img.shape
+        height, width = gray.shape
 
         h = round(height/quant_v)
         w = round(width/2)
@@ -36,16 +36,16 @@ def main():
         rect_sum = []
         t_tot = []
         for nw in range(2):
-            #linedw = cv2.line(horizontal_img, (w*nw, 0), (w*nw, width), (50), 2)
+            #linedw = cv2.line(horizontal_img, (w*nw, 0), (w*nw, height), (50), 2)
             rect.append([])
-           
-            rect[nw] = np.power(255-np.mean(np.mean(np.mean(horizontal_img[np.meshgrid(np.arange(0,height-h,h),np.arange(h,height,h)),w*nw:w*nw+w],2),1),0),5)/(math.pow(255,5)*quant_v)
-
+        
+            rect[nw] = np.power(255-np.mean(np.mean(np.mean(horizontal_img[np.meshgrid(np.arange(0,height-h,h),np.arange(h,height,h)),w*nw:w*nw+w],3),2),0),5)/(math.pow(255,5)*quant_v)
+            #print(rect[nw])
             rect_sum.append(sum(rect[nw]))
             t_tot.append(0)
-            for nv in range(quant_v):
+            for nv in range(quant_v-1):
+                #lined = cv2.line(linedw, (0, h*nv), (width, h*nv), (50), 2)
                 t_tot[nw] += rect[nw][nv]*t[nv]
-
         #t_tot = stereo_zipper(tonewindow(t_tot[0],4), tonewindow(t_tot[1],4))
         t_sum = sum(t_tot[0]) + sum(t_tot[1])
         t_tot = stereo_zipper(t_tot[0], t_tot[1])
